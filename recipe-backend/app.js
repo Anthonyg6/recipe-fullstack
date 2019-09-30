af09d7e34831e356e54c390ec4bfdc2598e5b221
Anthonyg6 <anthonygallegos6@gmail.com>
2019-09-29 19:51:44 -0600

    added cors headers and initiated post route

--- recipe-backend/app.js
+++ recipe-backend/app.js
@@ -1,34 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use(bodyParser.json());
-
-app.use("/api/recipes", (req, res, next) => {
-  const recipes = [
-    {
-      _id: "fdasdfasdfa",
-      title: "Pizza",
-      ingredients: "Tomato sauce, pepperoni, cheese, dough",
-      prepareTime: 15,
-      difficulty: 5
-    },
-    {
-      _id: "adfafsadfadsfads",
-      title: "Hamburger",
-      ingredients: "Beef, buns, veggies, cheese",
-      prepareTime: 10,
-      difficulty: 1
-    }
-  ];
-  res.status(200).json(recipes);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
+
+app.use(bodyParser.json());
 
 app.post("/api/recipes", (req, res, next) => {
   console.log(req.body);
   res.status(201).json({
-    message: "New recipe created"
+    message: "recipe created successfully"
   });
 });
 
+app.use("/api/recipes", (req, res, next) => {
+  const recipe = [
+    {
+      _id: "al;kdjf;akldsjfad",
+      title: "Pizza",
+      ingredients: "dough and sauce",
+      instructions: "bake the pizza",
+      prepareTime: 15,
+      difficulty: 1
+    }
+  ];
+  res.status(200).json(recipe);
+});
 module.exports = app;
